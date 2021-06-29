e54aaaf18e10f0bc50170d6e3e0fb6b66464b1f7
DeadCaramel <pal5686sanjana@gmail.com>
2021-06-29 15:19:10 +0800

    更新讲师ctrl和ossctrl

--- service/service-oss/src/main/java/com/papero/serviceoss/controller/OssController.java
+++ service/service-oss/src/main/java/com/papero/serviceoss/controller/OssController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/serviceoss")
+@RequestMapping("/eduoss")
 @Api(value="阿里云oss")
 public class OssController  {
 
