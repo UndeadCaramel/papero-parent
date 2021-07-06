84fafbd06fcf0d89315070e53f55e420d5b2d39c
undeadcaramel <jwpbt.4481@gmail.com>
2021-07-06 23:22:07 +0800

    修改课程分类路径

--- service/service-edu/src/main/java/com/papero/serviceedu/controller/EduSubjectController.java
+++ service/service-edu/src/main/java/com/papero/serviceedu/controller/EduSubjectController.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @since 2021-07-06
  */
 @RestController
-@RequestMapping("/eduservice/subject")
+@RequestMapping("/eduservice/edu-subject")
 @CrossOrigin
 public class EduSubjectController {
 
